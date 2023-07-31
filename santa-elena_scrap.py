9ab9d8df939e13ef4a7eff448717f5e838e0ac90
nktph <nkt.ph.coop@gmail.com>
2023-07-31 16:40:33 +0300

    fix for other cities

--- santa-elena_scrap.py
+++ santa-elena_scrap.py
@@ -3,25 +3,25 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-html = requests.get(f"https://www.santaelena.com.co/tiendas-pasteleria/tienda-medellin/").text
-soup = BeautifulSoup(html, 'html.parser')
-widgets = soup.find_all("div", re.compile("elementor-element elementor-element-....... elementor-widget "
-                                          "elementor-widget-text-editor"))
-
-shops_info = [widget.text.strip() for widget in widgets if 'Dirección' in str(widget)]
+cities = ['tienda-medellin', 'nuestra-pasteleria-en-barranquilla-santa-elena', 'tiendas-pastelerias-pereira', 'tienda-bogota', 'tienda-monteria']
 shops = []
-for info in shops_info:
-    info = info.split('\n')
-    shop = {
-        "name": "Sante Elena",
-        "address": info[0].split("Dirección: ")[1] if "Teléfono" in info[
-            1] else f"{info[0].split('Dirección: ')[1]} {info[1]}",
-        "latlon": "",
-        "phones": info[1].split("Teléfono: ")[1] if "Teléfono" in info[1] else info[2].split("Teléfono: ")[1],
-        "working_hours": info[4:] if 'Horario' in info[3] else info[3:]
-    }
-    shops.append(shop)
-    print(shop)
+for city in cities:
+    print(city)
+    html = requests.get(f"https://www.santaelena.com.co/tiendas-pasteleria/{city}/").text
+    soup = BeautifulSoup(html, 'html.parser')
+    widgets = soup.find_all("div", re.compile("elementor-element elementor-element-....... elementor-widget "
+                                              "elementor-widget-text-editor"))
+
+    shops_info = [widget.text.strip() for widget in widgets if 'Dirección' in str(widget)]
+    for info in shops_info:
+        shop = {
+            "name": "Sante Elena",
+            "address": info.split("Dirección:")[1].split("Teléfono:")[0].replace("\n", " "),
+            "latlon": "",
+            "phones": info.split("Teléfono:")[1].split("Horario de atención:")[0].replace("\n", " "),
+            "working_hours": info.split("Horario de atención:")[1].replace("\n", " ")
+        }
+        shops.append(shop)
 
 shops_json = json.dumps(shops)
 with open(f"shops.json", 'w', encoding="utf-8") as f:
